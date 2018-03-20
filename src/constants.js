5232a4cf69019f43a1bc06318d042fa5e3cef995
RakanNimer <rakannimer@gmail.com>
2018-03-20 16:10:20 +0200

    Run prettier on src + re-factor to use es modules.

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,65 @@
-/**
+export const getConstants = () => {
+  /**
  * Constants used in player.
  */
-var Constants = {
-	VERSION: '2.0.3',
-	NOTES: [],
-	CIRCLE_OF_FOURTHS: ['C', 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'Cb', 'Fb', 'Bbb', 'Ebb', 'Abb'],
-	CIRCLE_OF_FIFTHS: ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#', 'E#']
+  const constants = {
+    VERSION: "2.0.3",
+    NOTES: [],
+    CIRCLE_OF_FOURTHS: [
+      "C",
+      "F",
+      "Bb",
+      "Eb",
+      "Ab",
+      "Db",
+      "Gb",
+      "Cb",
+      "Fb",
+      "Bbb",
+      "Ebb",
+      "Abb"
+    ],
+    CIRCLE_OF_FIFTHS: [
+      "C",
+      "G",
+      "D",
+      "A",
+      "E",
+      "B",
+      "F#",
+      "C#",
+      "G#",
+      "D#",
+      "A#",
+      "E#"
+    ]
+  };
+
+  // Builds notes object for reference against binary values.
+  var allNotes = [
+    ["C"],
+    ["C#", "Db"],
+    ["D"],
+    ["D#", "Eb"],
+    ["E"],
+    ["F"],
+    ["F#", "Gb"],
+    ["G"],
+    ["G#", "Ab"],
+    ["A"],
+    ["A#", "Bb"],
+    ["B"]
+  ];
+  var counter = 0;
+
+  // All available octaves.
+  for (let i = -1; i <= 9; i++) {
+    allNotes.forEach(noteGroup => {
+      noteGroup.forEach(note => (constants.NOTES[counter] = note + i));
+      counter++;
+    });
+  }
+  return constants;
 };
 
-// Builds notes object for reference against binary values.
-var allNotes = [['C'], ['C#','Db'], ['D'], ['D#','Eb'], ['E'],['F'], ['F#','Gb'], ['G'], ['G#','Ab'], ['A'], ['A#','Bb'], ['B']];
-var counter = 0;
-
-// All available octaves.
-for (let i = -1; i <= 9; i++) {
-	allNotes.forEach(noteGroup => {
-		noteGroup.forEach(note => Constants.NOTES[counter] = note + i);
-		counter ++;
-	});
-}
-
-exports.Constants = Constants;
+export const Constants = getConstants();
